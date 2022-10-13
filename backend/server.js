a62b44de5f7eb72b8aec17d46c385f12ead4c85e
t18k <thirukumaran953@gmail.com>
2022-10-13 20:50:47 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
 const cors = require('cors')
@@ -19,9 +20,12 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
-app.use('/', (req, res)=>{
-	res.send('hello')
-})
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../frontend/build')))
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+}
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server started on port ${port}`))
