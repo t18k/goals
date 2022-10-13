e4c2d235d89fe7b5cbad00aa92e3d426e215bbf7
t18k <thirukumaran953@gmail.com>
2022-10-13 21:09:38 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,9 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../frontend/build')))
 	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
 }
+else {
+	app.get('/', (req, res) => res.send('please set to production'))
+}
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server started on port ${port}`))
